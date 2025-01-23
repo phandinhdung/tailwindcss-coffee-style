415f935d8ed9b3067076043dc6f0bb40ce3ffbba
Dinh-Dung-90 <phandinhdungcv@gmail.com>
2025-01-23 23:51:58 +0700

    xong footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xs': '480px'
+      },
       colors: {
         'coffee': {
           50: '#E8D6D0',
@@ -18,10 +21,15 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          from: {opacity: 0},
+          to: {opacity: 1}
         }
       },
       animation: {
-        slideDown: 'slideDown .4s ease-in-out',
+        slideDown: 'slideDown .5s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out'
       }
     },
   },
